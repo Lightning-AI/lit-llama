fba04ac86833e035601838e5de0129f69eda2c3b
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-03-24 01:41:29 -0400

    clean up

--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 import os
-import pickle
 import time
 from functools import partial
 
@@ -12,15 +11,13 @@ from torch.distributed.fsdp.wrap import transformer_auto_wrap_policy
 
 import models.llama as llama
 
-# TODO: properly structure this file into functions
+# TODO: clean up hyperparameters
 
 out_dir = "out"
 eval_interval = 2000
 log_interval = 1
 eval_iters = 200
-eval_only = False
 always_save_checkpoint = True
-dataset = "shakespeare"
 
 # TODO: hyperparameters
 gradient_accumulation_steps = 1
@@ -34,42 +31,125 @@ weight_decay = 1e-1
 beta1 = 0.9
 beta2 = 0.95
 grad_clip = 1.0
-compile = False  # use PyTorch 2.0 to compile the model to be faster
+compile = False
+
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.allow_tf32 = True
 
 
-auto_wrap_policy = partial(
-    transformer_auto_wrap_policy, transformer_layer_cls={llama.TransformerBlock}
-)
-strategy = FSDPStrategy(
-    auto_wrap_policy=auto_wrap_policy,
-    activation_checkpointing=llama.TransformerBlock,
-)
+def main():
+    auto_wrap_policy = partial(
+        transformer_auto_wrap_policy, transformer_layer_cls={llama.TransformerBlock}
+    )
+    strategy = FSDPStrategy(
+        auto_wrap_policy=auto_wrap_policy,
+        activation_checkpointing=llama.TransformerBlock,
+    )
 
-fabric = L.Fabric(
-    accelerator="cuda",
-    devices=4,
-    precision="bf16",
-    strategy=strategy,
-)
-fabric.launch()
-fabric.seed_everything(1337 + fabric.global_rank)
-
-
-if fabric.global_rank == 0:
-    os.makedirs(out_dir, exist_ok=True)
-
-torch.backends.cuda.matmul.allow_tf32 = True  # allow tf32 on matmul
-torch.backends.cudnn.allow_tf32 = True  # allow tf32 on cudnn
+    fabric = L.Fabric(
+        accelerator="cuda",
+        devices=4,
+        precision="bf16",
+        strategy=strategy,
+    )
+    fabric.launch()
+    fabric.seed_everything(1337 + fabric.global_rank)
 
 
-# TODO: clean this up
-data_dir = os.path.join("data", dataset)
-train_data = np.memmap(os.path.join(data_dir, "train.bin"), dtype=np.uint16, mode="r")
-val_data = np.memmap(os.path.join(data_dir, "val.bin"), dtype=np.uint16, mode="r")
+    if fabric.global_rank == 0:
+        os.makedirs(out_dir, exist_ok=True)
 
 
-def get_batch(split):
-    data = train_data if split == "train" else val_data
+    train_data, val_data = load_datasets()
+
+    # TODO: replace with Nano-LLaMA
+    llama_config = llama.LLAMA_CONFIG_DICT["7B"]
+    with fabric.device:
+        model = llama.LLaMA(llama_config)
+
+    if compile:
+        model = torch.compile(model)
+
+    model = fabric.setup_module(model)
+
+    # TODO: AdamW from paper
+    optimizer = torch.optim.Adam(model.parameters())
+    optimizer = fabric.setup_optimizers(optimizer)
+
+    train(fabric, model, optimizer, train_data, val_data)
+
+
+def train(fabric, model, optimizer, train_data, val_data):
+    t0 = time.time()
+    iter_num = 0
+    best_val_loss = 1e9
+
+    while True:
+        # TODO: add learning rate scheduling
+
+        # evaluate the loss on train/val sets and write checkpoints
+        if iter_num > 0 and iter_num % eval_interval == 0 and fabric.global_rank == 0:
+            val_loss = validate(fabric, model, val_data)
+            fabric.print(f"step {iter_num}: val loss {val_loss:.4f}")
+            if val_loss < best_val_loss or always_save_checkpoint:
+                best_val_loss = val_loss
+                # TODO: Save with Fabric
+                # checkpoint = {
+                #     "model": model.state_dict(),
+                #     "optimizer": optimizer.state_dict(),
+                #     "iter_num": iter_num,
+                #     "best_val_loss": best_val_loss,
+                # }
+                # print(f"saving checkpoint to {out_dir}")
+                # torch.save(checkpoint, os.path.join(out_dir, 'ckpt.pt'))
+
+        # TODO: efficient gradient accumulation
+        # TODO: Should we do gradient accumulation at all?
+        for _ in range(gradient_accumulation_steps):
+            input_ids, targets = get_batch(train_data, device=fabric.device)
+            logits = model(input_ids)
+            loss = F.cross_entropy(
+                logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1
+            )
+            fabric.backward(loss)
+
+        # TODO: Gradient clipping
+        # if grad_clip != 0.0:
+        #     fabric.clip_gradients(model, optimizer, max_norm=grad_clip)
+
+        optimizer.step()
+        optimizer.zero_grad(set_to_none=True)
+
+        # timing and logging
+        t1 = time.time()
+        dt = t1 - t0
+        t0 = t1
+        if iter_num % log_interval == 0 and fabric.global_rank == 0:
+            fabric.print(f"iter {iter_num}: loss {loss.item():.4f}, time {dt*1000:.2f}ms")
+        iter_num += 1
+
+        if iter_num > max_iters:
+            break
+
+
+@torch.no_grad()
+def validate(fabric, model, val_data):
+    out = {}
+    model.eval()
+    losses = torch.zeros(eval_iters)
+    for k in range(eval_iters):
+        input_ids, targets = get_batch(val_data, device=fabric.device)
+        logits = model(input_ids)
+        loss = F.cross_entropy(
+            logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1
+        )
+        losses[k] = loss.item()
+        out = losses.mean()
+    model.train()
+    return out
+
+
+def get_batch(data, device):
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack(
         [torch.from_numpy((data[i : i + block_size]).astype(np.int64)) for i in ix]
@@ -81,116 +161,18 @@ def get_batch(split):
         ]
     )
 
-    # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
-    x, y = x.pin_memory().to(fabric.device, non_blocking=True), y.pin_memory().to(
-        fabric.device, non_blocking=True
+    # TODO: Use fabric here
+    x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(
+        device, non_blocking=True
     )
     return x, y
 
 
-# init these up here, can override if init_from='resume' (i.e. from a checkpoint)
-iter_num = 0
-best_val_loss = 1e9
-
-# attempt to derive vocab_size from the dataset
-meta_path = os.path.join(data_dir, "meta.pkl")
-meta_vocab_size = None
-if os.path.exists(meta_path):
-    with open(meta_path, "rb") as f:
-        meta = pickle.load(f)
-    meta_vocab_size = meta["vocab_size"]
-    print(f"found vocab_size = {meta_vocab_size} (inside {meta_path})")
+def load_datasets(data_dir="data/shakespeare"):
+    train_data = np.memmap(os.path.join(data_dir, "train.bin"), dtype=np.uint16, mode="r")
+    val_data = np.memmap(os.path.join(data_dir, "val.bin"), dtype=np.uint16, mode="r")
+    return train_data, val_data
 
 
-# TODO: replace with Nano-LLaMA
-print("Initializing a new model from scratch")
-llama_config = llama.LLAMA_CONFIG_DICT["7B"]
-with fabric.device:
-    model = llama.LLaMA(llama_config)
-
-
-# compile the model
-if compile:
-    model = torch.compile(model)  # requires PyTorch 2.0
-
-model = fabric.setup_module(model)
-
-# TODO: AdamW from paper
-optimizer = torch.optim.Adam(model.parameters())
-optimizer = fabric.setup_optimizers(optimizer)
-
-# TODO: clean up
-@torch.no_grad()
-def estimate_loss():
-    out = {}
-    model.eval()
-    for split in ["train", "val"]:
-        losses = torch.zeros(eval_iters)
-        for k in range(eval_iters):
-            input_ids, targets = get_batch(split)
-            logits = model(input_ids)
-            loss = F.cross_entropy(
-                logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1
-            )
-            losses[k] = loss.item()
-        out[split] = losses.mean()
-    model.train()
-    return out
-
-
-# training loop
-t0 = time.time()
-local_iter_num = 0  # number of iterations in the lifetime of this process
-
-while True:
-    # TODO: add learning rate scheduling
-
-    # evaluate the loss on train/val sets and write checkpoints
-    if iter_num > 0 and iter_num % eval_interval == 0 and fabric.global_rank == 0:
-        losses = estimate_loss()
-        print(
-            f"step {iter_num}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}"
-        )
-        if losses["val"] < best_val_loss or always_save_checkpoint:
-            best_val_loss = losses["val"]
-            checkpoint = {
-                "model": model.state_dict(),
-                "optimizer": optimizer.state_dict(),
-                "iter_num": iter_num,
-                "best_val_loss": best_val_loss,
-            }
-
-            # TODO: Save with Fabric
-            # print(f"saving checkpoint to {out_dir}")
-            # torch.save(checkpoint, os.path.join(out_dir, 'ckpt.pt'))
-    if iter_num == 0 and eval_only:
-        break
-
-    # TODO: efficient gradient accumulation
-    # TODO: Should we do gradient accumulation at all?
-    for micro_step in range(gradient_accumulation_steps):
-        input_ids, targets = get_batch("train")
-        logits = model(input_ids)
-        loss = F.cross_entropy(
-            logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1
-        )
-        fabric.backward(loss)
-
-    # TODO: Gradient clipping
-    # if grad_clip != 0.0:
-    #     fabric.clip_gradients(model, optimizer, max_norm=grad_clip)
-
-    optimizer.step()
-    optimizer.zero_grad(set_to_none=True)
-
-    # timing and logging
-    t1 = time.time()
-    dt = t1 - t0
-    t0 = t1
-    if iter_num % log_interval == 0 and fabric.global_rank == 0:
-        fabric.print(f"iter {iter_num}: loss {loss.item():.4f}, time {dt*1000:.2f}ms")
-    iter_num += 1
-    local_iter_num += 1
-
-    if iter_num > max_iters:
-        break
+if __name__ == "__main__":
+    main()
