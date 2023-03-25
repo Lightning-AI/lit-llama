e933b89537973952fec64cdc46778514f10e30ce
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-03-25 09:36:38 -0400

    function

--- scripts/convert_checkpoint.py
+++ scripts/convert_checkpoint.py
@@ -7,15 +7,41 @@ Sample usage:
 ```bash
 python -m scripts.convert_checkpoint -h
 
-python -m scripts.convert_checkpoint meta_weights_for_meta_model converted
+python -m scripts.convert_checkpoint converted
 ```
 """
+
+def convert_state_dict(state_dict):
+    converted = {}
+    converted["transformer.wte.weight"] = state_dict["tok_embeddings.weight"]
+    converted["lm_head.weight"] = state_dict["output.weight"]
+    converted["transformer.ln_f.scale"] = state_dict["norm.weight"]  # todo: correct?
+
+    for key in [k for k in state_dict if k.startswith("layers")]:
+        layer_idx = key.split(".")[1]
+
+        # attention
+        # the wq, wk, wv from the FB model are stacked in our model as c_attn
+        converted[f"transformer.h.{layer_idx}.attn.c_attn.weight"] = torch.cat((
+            state_dict[f"layers.{layer_idx}.attention.wq.weight"],
+            state_dict[f"layers.{layer_idx}.attention.wk.weight"],
+            state_dict[f"layers.{layer_idx}.attention.wv.weight"],
+        ))
+        converted[f"transformer.h.{layer_idx}.attn.c_proj.weight"] = state_dict[f"layers.{layer_idx}.attention.wo.weight"]
+        # mlp
+        converted[f"transformer.h.{layer_idx}.mlp.c_fc1.weight"] = state_dict[f"layers.{layer_idx}.feed_forward.w1.weight"]
+        converted[f"transformer.h.{layer_idx}.mlp.c_proj.weight"] = state_dict[f"layers.{layer_idx}.feed_forward.w2.weight"]
+        converted[f"transformer.h.{layer_idx}.mlp.c_fc2.weight"] = state_dict[f"layers.{layer_idx}.feed_forward.w3.weight"]
+        # rms norm
+        converted[f"transformer.h.{layer_idx}.rms_1.scale"] = state_dict[f"layers.{layer_idx}.attention_norm.weight"]
+        converted[f"transformer.h.{layer_idx}.rms_2.scale"] = state_dict[f"layers.{layer_idx}.ffn_norm.weight"]
+    return converted
 
 
 def meta_weights_for_nano_model(
     *,
     output_dir: Path,
-    ckpt_dir: Path = Path("/srv/data/checkpoints/llama/raw/7B"),
+    ckpt_dir: Path = Path("/srv/data/checkpoints/llama/raw"),
     tokenizer_path: Path = Path("/srv/data/checkpoints/llama/raw/tokenizer.model"),
     model_size: str = "7B",
 ):
@@ -26,6 +52,7 @@ def meta_weights_for_nano_model(
     import shutil
 
     output_dir = output_dir / model_size
+    ckpt_dir = ckpt_dir / model_size
     os.makedirs(output_dir, exist_ok=True)
 
     if "tokenizer.model" not in os.listdir(output_dir):
@@ -35,33 +62,8 @@ def meta_weights_for_nano_model(
     checkpoint_files = sorted(ckpt_dir.glob("*.pth"))
    
     for file in tqdm(checkpoint_files, total=len(checkpoint_files)):
-        converted = {}
         checkpoint = torch.load(file, map_location="cpu")
-        
-        converted["transformer.wte.weight"] = checkpoint["tok_embeddings.weight"]
-        converted["lm_head.weight"] = checkpoint["output.weight"]
-        converted["transformer.ln_f.scale"] = checkpoint["norm.weight"]  # todo: correct?
-
-        for key in [k for k in checkpoint if k.startswith("layers")]:
-            layer_idx = key.split(".")[1]
-
-            # attention
-            # the wq, wk, wv from the FB model are stacked in our model as c_attn
-            converted[f"transformer.h.{layer_idx}.attn.c_attn.weight"] = torch.cat((
-                # TODO: is order correct?
-                checkpoint[f"layers.{layer_idx}.attention.wq.weight"],
-                checkpoint[f"layers.{layer_idx}.attention.wk.weight"],
-                checkpoint[f"layers.{layer_idx}.attention.wv.weight"],
-            ))
-            converted[f"transformer.h.{layer_idx}.attn.c_proj.weight"] = checkpoint[f"layers.{layer_idx}.attention.wo.weight"]
-            # mlp
-            converted[f"transformer.h.{layer_idx}.mlp.c_fc1.weight"] = checkpoint[f"layers.{layer_idx}.feed_forward.w1.weight"]
-            converted[f"transformer.h.{layer_idx}.mlp.c_proj.weight"] = checkpoint[f"layers.{layer_idx}.feed_forward.w2.weight"]
-            converted[f"transformer.h.{layer_idx}.mlp.c_fc2.weight"] = checkpoint[f"layers.{layer_idx}.feed_forward.w3.weight"]
-            # rms norm
-            converted[f"transformer.h.{layer_idx}.rms_1.scale"] = checkpoint[f"layers.{layer_idx}.attention_norm.weight"]
-            converted[f"transformer.h.{layer_idx}.rms_2.scale"] = checkpoint[f"layers.{layer_idx}.ffn_norm.weight"]
-
+        converted = convert_state_dict(checkpoint)
         torch.save(converted, Path(output_dir, file.name))
 
 
