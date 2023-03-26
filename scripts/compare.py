c9042cb77e7f0d4e9b49619c52716dd11c338817
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-03-26 02:58:11 -0400

    add old version and show divergence

--- scripts/compare.py
+++ scripts/compare.py
@@ -4,6 +4,53 @@ import sys
 import torch
 
 
+def build_rope_cache_old(seq_len, n_elem, dtype, device, base=10000):
+    """This is the `build_rope_cache` implementation we initially intended to use, but it is
+    numerically not exactly equivalent to the one in the Meta model. We keep it here for posterity.
+
+    Derived from: https://github.com/labmlai/annotated_deep_learning_paper_implementations/blob/master/labml_nn/transformers/rope/__init__.py
+    MIT License: https://github.com/labmlai/annotated_deep_learning_paper_implementations/blob/master/license
+    """
+    # $\Theta = {\theta_i = 10000^{\frac{2(i-1)}{d}}, i \in [1, 2, ..., \frac{d}{2}]}$
+    theta = 1. / (base ** (torch.arange(0, n_elem, 2, dtype=dtype, device=device) / n_elem))
+
+    # Create position indexes `[0, 1, ..., seq_len - 1]`
+    seq_idx = torch.arange(seq_len, device=device, dtype=dtype)
+
+    # Calculate the product of position index and $\theta_i$
+    idx_theta = torch.outer(seq_idx, theta)
+
+    # Concatenate so that for row $m$ we have
+    # $[m \theta_0, m \theta_1, ..., m \theta_{\frac{d}{2}}, m \theta_0, m \theta_1, ..., m \theta_{\frac{d}{2}}]$
+    idx_theta2 = torch.cat([idx_theta, idx_theta], dim=1)
+
+    # Cache them
+    cos_cache = idx_theta2.cos()[None, None, :, :]
+    sin_cache = idx_theta2.sin()[None, None, :, :]
+
+    return torch.stack((cos_cache, sin_cache), dim=0)
+
+
+def rotate_neg_half(x: torch.Tensor):
+    # $\frac{d}{2}$
+    d_2 = x.shape[-1] // 2
+    # Calculate $[-x^{(\frac{d}{2} + 1)}, -x^{(\frac{d}{2} + 2)}, ..., -x^{(d)}, x^{(1)}, x^{(2)}, ..., x^{(\frac{d}{2})}]$
+    return torch.cat([-x[:, :, :, d_2:], x[:, :, :, :d_2]], dim=-1)
+
+
+def apply_rope_old(x: torch.Tensor, rope_cache):
+    """This is the `apply_rope` implementation we initially intended to use, but it is
+    numerically not exactly equivalent to the one in the Meta model. We keep it here for posterity.
+    """
+    neg_half_x = rotate_neg_half(x)
+    cos, sin = rope_cache
+    # truncate to support variable sizes
+    T = x.size(2)
+    cos = cos[:, :, :T]
+    sin = sin[:, :, :T]
+    return (x * cos) + (neg_half_x * sin)
+
+
 def compare_rope():
     bs, seq_len, n_head, n_embed = 1, 6, 2, 8
     x = torch.randint(0, 10000, size=(bs, seq_len, n_head, n_embed // n_head)).float()
@@ -17,6 +64,12 @@ def compare_rope():
 
     apply_rope_matches = torch.equal(llama_x_rope, orig_llama_x_rope)
     print(f"Comparing apply rope:\t\t{'OK' if apply_rope_matches else 'KO'}")
+
+    # For posterity, we show here that our older implementation we initially wanted to use
+    # is not numerically equivalent to Meta's rope implementation
+    llama_rope_cache_old = build_rope_cache_old(seq_len, n_embed // n_head, dtype=x.dtype, device=x.device)
+    llama_x_rope_old = apply_rope_old(x, llama_rope_cache_old)
+    assert not torch.allclose(llama_x_rope_old, orig_llama_x_rope)
 
 
 def compare_rmsnorm():
