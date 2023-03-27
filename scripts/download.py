5325e07d4a79d601dcf43f7471d9812ab424c6ee
Carlos Mocholí <carlossmocholi@gmail.com>
2023-03-27 13:45:31 -0400

    Fix mypy

--- scripts/download.py
+++ scripts/download.py
@@ -1,12 +1,12 @@
 import os
-import urllib
+from urllib.request import urlretrieve
 
 
-def download_original(wd: str):
+def download_original(wd: str) -> None:
     filepath = os.path.join(wd, "original_model.py")
     if not os.path.isfile(filepath):
         print(f"Downloading original implementation to {filepath!r}")
-        urllib.request.urlretrieve(
+        urlretrieve(
             url="https://gist.githubusercontent.com/lantiga/fd36849fb1c498da949a0af635318a7b/raw/fe9561d1abd8d2c61c82dd62155fe98d3ac74c43/llama_model.py",
             filename="original_model.py",
         )
