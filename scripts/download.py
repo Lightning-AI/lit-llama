e4639312603a370fdf1df9178a81ac0cc8c66606
Carlos Mocholí <carlossmocholi@gmail.com>
2023-03-27 08:26:53 -0400

    TODO

--- scripts/download.py
+++ scripts/download.py
@@ -7,6 +7,7 @@ def download_original(wd: str):
     if not os.path.isfile(filepath):
         print(f"Downloading original implementation to {filepath!r}")
         urllib.request.urlretrieve(
+            # TODO: update SHA
             url="https://gist.githubusercontent.com/lantiga/fd36849fb1c498da949a0af635318a7b/raw/c4509e48a53ebb6a195a6f073b5267a69e47b45a/llama_model.py",
             filename="original_model.py",
         )
