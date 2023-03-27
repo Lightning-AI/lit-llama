df16e49f2a13f3984a8e2927ce3865acfbae5b77
Luca Antiga <luca@lightning.ai>
2023-03-28 00:32:27 +0200

    Remove email

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version='0.1.0',
     description='Implementation of the LLaMA language model',
     author='Lightning AI',
-    author_email='will@lightning.ai',
     url='https://github.com/lightning-AI/lit-llama',
     # install_requires=['lightning'],
     packages=find_packages(),
