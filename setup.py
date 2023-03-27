a42aac19a0e0e3eeabcdbee72b478c79119415c5
Luca Antiga <luca@lightning.ai>
2023-03-28 00:52:05 +0200

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,11 @@ setup(
     description='Implementation of the LLaMA language model',
     author='Lightning AI',
     url='https://github.com/lightning-AI/lit-llama',
-    # install_requires=['lightning'],
+    install_requires=[
+        "torch>=2.0.0",
+        "lightning>=2.0.0",
+        "sentencepiece",
+        "bitsandbytes",
+    ],
     packages=find_packages(),
 )
