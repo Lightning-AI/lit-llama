3150d533eddb540a620a6a99f7095eda0737f4a0
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-03-28 08:57:19 -0400

    fix weights in test

--- tests/test_model.py
+++ tests/test_model.py
@@ -54,6 +54,8 @@ def test_to_orig_llama(orig_llama) -> None:
     )
 
     llama_model = lit_llama.LLaMA(llama_config)
+    # init all weights
+    llama_model.apply(llama_model._init_weights)
     orig_llama_model = orig_llama.Transformer(orig_llama_config)
 
     copy_weights(llama_model, orig_llama_model)
