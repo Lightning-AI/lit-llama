e2def3f61116a8a893e01a44793155adae088e43
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-04-12 11:18:45 -0400

    clean up

--- generate_adapter.py
+++ generate_adapter.py
@@ -15,43 +15,42 @@ from scripts.prepare_alpaca import generate_prompt
 
 
 def main(
-    prompt: str = "What food do lamas eat?",
+    prompt: str,
+    adapter_path: Path,
     *,
     input: str = "",
+    pretrained_path: Optional[Path] = None,
+    tokenizer_path: Optional[Path] = None,
+    quantize: Optional[str] = None,
+    dtype: Optional[str] = None,
     max_new_tokens: int = 100,
     top_k: int = 200,
     temperature: float = 0.8,
     accelerator: str = "auto",
-    pretrained_path: Optional[Path] = None,
-    adapter_path: Optional[Path] = None,
-    tokenizer_path: Optional[Path] = None,
-    model_size: str = "7B",
-    dtype: Optional[str] = None,
-    quantize: Optional[str] = None,
 ) -> None:
     """Generates a response based on a given instruction and an optional input.
-    This script is mean to work with checkpoints from an instruction-tuned model such as LLaMA-Adapter.
+    This script will only work with checkpoints from the instruction-tuned LLaMA-Adapter model.
     See `finetune_adapter.py`.
 
     Args:
         prompt: The prompt/instruction (Alpaca style).
+        adapter_path: Path to the checkpoint with trained adapter weights, which are the output of
+            `finetune_adapter.py`.
         input: Optional input (Alpaca style).
+        pretrained_path: The path to the checkpoint with pretrained LLaMA weights.
+        tokenizer_path: The tokenizer path to load.
+        quantize: Whether to quantize the model and using which method:
+            ``"llm.int8"``: LLM.int8() mode,
+            ``"gptq.int4"``: GPTQ 4-bit mode.
         max_new_tokens: The number of generation steps to take.
         top_k: The number of top most probable tokens to consider in the sampling process.
         temperature: A value controlling the randomness of the sampling process. Higher values result in more random
             samples.
         accelerator: The hardware to run on. Possible choices are:
             ``"cpu"``, ``"cuda"``, ``"mps"``, ``"gpu"``, ``"tpu"``, ``"auto"``.
-        pretrained_path: The path to the checkpoint with pretrained LLaMA weights.
-        adapter_path: Path to the checkpoint with trained adapter weights, which are the output of
-            `finetune_adapter.py`.
-        tokenizer_path: The tokenizer path to load.
-        quantize: Whether to quantize the model and using which method:
-            ``"llm.int8"``: LLM.int8() mode,
-            ``"gptq.int4"``: GPTQ 4-bit mode.
     """
     if not pretrained_path:
-        pretrained_path = Path(f"./checkpoints/lit-llama/{model_size}/state_dict.pth")
+        pretrained_path = Path(f"./checkpoints/lit-llama/7B/state_dict.pth")
     if not tokenizer_path:
         tokenizer_path = Path("./checkpoints/lit-llama/tokenizer.model")
     assert pretrained_path.is_file()
@@ -71,9 +70,11 @@ def main(
     ):
         print("Loading model ...", file=sys.stderr)
         t0 = time.time()
-        model = LLaMA(LLaMAConfig())
+        model = LLaMA(LLaMAConfig())  # TODO: Support different model sizes
+        # 1. Load the pretrained weights
         pretrained_checkpoint = torch.load(pretrained_path)
         model.load_state_dict(pretrained_checkpoint, strict=False)
+        # 2. Load the fine-tuned adapter weights
         adapter_checkpoint = torch.load(adapter_path)
         model.load_state_dict(adapter_checkpoint, strict=False)
         print(f"Time to load model: {time.time() - t0:.02f} seconds.", file=sys.stderr)
@@ -85,14 +86,13 @@ def main(
     encoded_prompt = tokenizer.encode(prompt, bos=True, eos=False, device=fabric.device)
     encoded_prompt = encoded_prompt[None, :]  # add batch dimension
 
-    t0 = time.perf_counter()
-
     sample = {"instruction": prompt, "input": input}
     prompt = generate_prompt(sample)
     encoded = tokenizer.encode(prompt, bos=True, eos=False)
     encoded = encoded[None, :]  # add batch dimension
     encoded = encoded.to(model.device)
 
+    t0 = time.perf_counter()
     output = generate(
         model,
         idx=encoded,
@@ -101,8 +101,8 @@ def main(
         temperature=temperature,
         top_k=top_k,
     )
+    # The end of the response is where the model generates the EOS token
     output = truncate_output_to_eos(output[0].cpu(), tokenizer.eos_id)
-    
     output = tokenizer.decode(output)
     output = output.split("### Response:")[1].strip()
 
@@ -114,7 +114,6 @@ def main(
 
 
 def truncate_output_to_eos(output, eos_id):
-    # The end of the response is where the model generates the EOS token
     # TODO: Make this more efficient, terminate generation early
     try:
         eos_pos = output.tolist().index(eos_id)
