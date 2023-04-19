2b8d60053ccca6ff9b9a623ece968f0a36ae56d3
Aniket Maurya <theaniketmaurya@gmail.com>
2023-04-19 11:49:34 +0000

    rename keys

--- scripts/prepare_dolly.py
+++ scripts/prepare_dolly.py
@@ -94,7 +94,7 @@ def prepare_sample(example: dict, tokenizer: Tokenizer, max_length: int, mask_in
     in the label that correspond to the original input prompt get masked out (default).
     """
     full_prompt = generate_prompt(example)
-    full_prompt_and_response = full_prompt + example["response"]
+    full_prompt_and_response = full_prompt + example["output"]
     encoded_full_prompt = tokenize(tokenizer, full_prompt, max_length=max_length, eos=False)
     encoded_full_prompt_and_response = tokenize(tokenizer, full_prompt_and_response, eos=True, max_length=max_length)
 
@@ -114,14 +114,14 @@ def generate_prompt(example):
     """Generates a standardized message to prompt the model with an instruction, optional input and a
     'response' field."""
 
-    if example["context"]:
+    if example["input"]:
         return (
-            f"Below is an instruction that describes a {example['category']} task, paired with an input that provides further context. "
+            f"Below is an instruction that describes a task, paired with an input that provides further context. "
             "Write a response that appropriately completes the request.\n\n"
-            f"### Instruction:\n{example['instruction']}\n\n### Input:\n{example['context']}\n\n### Response:"
+            f"### Instruction:\n{example['instruction']}\n\n### Input:\n{example['input']}\n\n### Response:"
         )
     return (
-        f"Below is an instruction that describes a {example['category']} task. "
+        f"Below is an instruction that describes a task. "
         "Write a response that appropriately completes the request.\n\n"
         f"### Instruction:\n{example['instruction']}\n\n### Response:"
     )
