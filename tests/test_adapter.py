ba6cd1bdedac73f9bd42e70ca0fcc09d56d6c405
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-04-19 10:42:09 -0400

    update

--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -1,21 +1,22 @@
 import torch
 from dataclasses import asdict
+import pytest
 
 
-def test_config_identical(lit_llama):
+@pytest.mark.parametrize("model_size", ["7B", "13B", "30B", "65B"])
+def test_config_identical(model_size, lit_llama):
     import lit_llama.adapter as llama_adapter
     import lit_llama.model as llama
     from lit_llama.utils import EmptyInitOnDevice
 
-    llama_config = asdict(llama.LLaMAConfig())
-    adapter_config = asdict(llama_adapter.LLaMAConfig())
+    llama_config = asdict(llama.LLaMAConfig.from_name(model_size))
+    adapter_config = asdict(llama_adapter.LLaMAConfig.from_name(model_size))
 
     del adapter_config["adapter_prompt_length"]
     del adapter_config["adapter_start_layer"]
     assert adapter_config == llama_config
 
-    with EmptyInitOnDevice(device=torch.device("cpu")):
-        llama_model = llama.LLaMA.from_name("7B")
-        adapter_model = llama_adapter.LLaMA.from_name("7B")
-
+    with EmptyInitOnDevice():
+        llama_model = llama.LLaMA.from_name(model_size)
+        adapter_model = llama_adapter.LLaMA.from_name(model_size)
         assert llama_model.lm_head.weight.shape == adapter_model.lm_head.weight.shape
