841555968de3157e9a729ecaf6ed75d173433fa1
rasbt <mail@sebastianraschka.com>
2023-04-26 15:06:03 -0500

    add missing state dict

--- finetune_full.py
+++ finetune_full.py
@@ -76,7 +76,8 @@ def main(
     train(fabric, model, optimizer, train_data, val_data, out_dir)
 
     # Save the final checkpoint at the end of training
-    fabric.save(os.path.join(out_dir, "lit-llama-full-finetuned.pth"), checkpoint)
+    checkpoint = lora_state_dict(model)
+    fabric.save(os.path.join(out_dir, "lit-llama-full-finetuned.pth"), model.state_dict())
 
 
 def train(
