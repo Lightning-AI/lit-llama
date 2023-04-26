0c1f04720560c9491d35cc4fd71c510184c39cf2
Luca Antiga <luca@lightning.ai>
2023-04-26 18:44:39 +0200

    Reorder classes and remove commented code

--- lit_llama/packed_dataset.py
+++ lit_llama/packed_dataset.py
@@ -4,7 +4,6 @@
 
 import os
 import struct
-import random
 
 import numpy as np
 from torch.utils.data import IterableDataset, get_worker_info
@@ -31,109 +30,6 @@ def code(dtype):
 
 HDR_MAGIC = b"LITPKDS"
 HDR_SIZE = 24  # bytes
-
-
-class PackedDatasetIterator:
-    def __init__(self, filenames, n_chunks, block_size, seed, shuffle):
-        self._seed = seed
-        self._shuffle = shuffle
-        self._rng = np.random.default_rng(seed) if shuffle else None
-        self._block_idxs = None
-
-        # TODO: instead of filenames, we could have a single text stream
-        #       (or text file) with the sequence of all files to be
-        #       fetched/loaded.
-        self._filenames = filenames
-        self._file_idx = 0
-
-        self._n_chunks = n_chunks
-
-        self._dtype = None
-        self._block_size = block_size
-        self._n_blocks = None
-
-        self._mmaps = []
-        self._buffers = []
-
-        self._block_idxs = []
-        self._curr_idx = 0
-
-        self._load_n_chunks()
-
-    def _read_header(self, path):
-        with open(path, "rb") as f:
-            magic = f.read(len(HDR_MAGIC))
-            assert magic == HDR_MAGIC, "File doesn't match expected format."
-            version = struct.unpack("<Q", f.read(8))
-            assert (1,) == version
-            (dtype_code,) = struct.unpack("<B", f.read(1))
-            dtype = dtypes[dtype_code]
-            (chunk_size,) = struct.unpack("<Q", f.read(8))
-        return dtype, chunk_size
-
-    def _close_mmaps(self):
-        for mmap in self._mmaps:
-            mmap._mmap.close()
-
-    def _load_n_chunks(self):
-        self._close_mmaps()
-
-        if self._n_chunks > len(self._filenames[self._file_idx :]):
-            raise StopIteration
-
-        for i in range(self._n_chunks):
-            filename = self._filenames[self._file_idx + i]
-            if self._dtype is None:
-                self._dtype, self._chunk_size = self._read_header(
-                    filename
-                )
-                self._n_blocks = self._chunk_size // self._block_size
-            # TODO: check header matches with previous files
-            mmap = np.memmap(filename, mode="r", order="C", offset=HDR_SIZE)
-            self._mmaps.append(mmap)
-            self._buffers.append(memoryview(mmap))
-
-        self._file_idx += self._n_chunks
-        n_all_blocks = self._n_chunks * self._n_blocks
-
-        self._block_idxs = (
-            self._rng.permutation(n_all_blocks)
-            if self._shuffle
-            else range(n_all_blocks)
-        )
-
-        self._curr_idx = 0
-
-    # def __getstate__(self):
-    #     # TODO: update
-    #     return self._filenames, self._n_chunks, self._seed
-
-    # def __setstate__(self, state):
-    #     # TODO: update
-    #     # to make it deterministic we'll have to rewind the rng
-    #     self._filenames, self._n_chunks, self._seed = state
-
-    def __del__(self):
-        self._close_mmaps()
-        del self._mmaps
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self._curr_idx >= len(self._block_idxs):
-            self._load_n_chunks()
-            # TODO: trigger fetching next next n_chunks if remote
-        block_idx = self._block_idxs[self._curr_idx]
-        chunk_id = block_idx // self._n_blocks
-        buffer = self._buffers[chunk_id]
-        elem_id = (block_idx % self._n_blocks) * self._block_size
-        offset = np.dtype(self._dtype).itemsize * elem_id
-        arr = np.frombuffer(
-            buffer, dtype=self._dtype, count=self._block_size, offset=offset
-        )
-        self._curr_idx += 1
-        return arr
 
 
 class PackedDataset(IterableDataset):
@@ -235,3 +131,99 @@ class PackedDatasetBuilder(object):
 
     def write_reminder(self):
         self._write_chunk()
+
+
+class PackedDatasetIterator:
+    def __init__(self, filenames, n_chunks, block_size, seed, shuffle):
+        self._seed = seed
+        self._shuffle = shuffle
+        self._rng = np.random.default_rng(seed) if shuffle else None
+        self._block_idxs = None
+
+        # TODO: instead of filenames, we could have a single text stream
+        #       (or text file) with the sequence of all files to be
+        #       fetched/loaded.
+        self._filenames = filenames
+        self._file_idx = 0
+
+        self._n_chunks = n_chunks
+
+        self._dtype = None
+        self._block_size = block_size
+        self._n_blocks = None
+
+        self._mmaps = []
+        self._buffers = []
+
+        self._block_idxs = []
+        self._curr_idx = 0
+
+        self._load_n_chunks()
+
+    def _read_header(self, path):
+        with open(path, "rb") as f:
+            magic = f.read(len(HDR_MAGIC))
+            assert magic == HDR_MAGIC, "File doesn't match expected format."
+            version = struct.unpack("<Q", f.read(8))
+            assert (1,) == version
+            (dtype_code,) = struct.unpack("<B", f.read(1))
+            dtype = dtypes[dtype_code]
+            (chunk_size,) = struct.unpack("<Q", f.read(8))
+        return dtype, chunk_size
+
+    def _close_mmaps(self):
+        for mmap in self._mmaps:
+            mmap._mmap.close()
+
+    def _load_n_chunks(self):
+        self._close_mmaps()
+
+        if self._n_chunks > len(self._filenames[self._file_idx :]):
+            raise StopIteration
+
+        for i in range(self._n_chunks):
+            filename = self._filenames[self._file_idx + i]
+            if self._dtype is None:
+                self._dtype, self._chunk_size = self._read_header(
+                    filename
+                )
+                self._n_blocks = self._chunk_size // self._block_size
+            # TODO: check header matches with previous files
+            mmap = np.memmap(filename, mode="r", order="C", offset=HDR_SIZE)
+            self._mmaps.append(mmap)
+            self._buffers.append(memoryview(mmap))
+
+        self._file_idx += self._n_chunks
+        n_all_blocks = self._n_chunks * self._n_blocks
+
+        self._block_idxs = (
+            self._rng.permutation(n_all_blocks)
+            if self._shuffle
+            else range(n_all_blocks)
+        )
+
+        self._curr_idx = 0
+
+    def __del__(self):
+        self._close_mmaps()
+        del self._mmaps
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._curr_idx >= len(self._block_idxs):
+            self._load_n_chunks()
+            # TODO: trigger fetching next next n_chunks if remote
+        block_idx = self._block_idxs[self._curr_idx]
+        chunk_id = block_idx // self._n_blocks
+        buffer = self._buffers[chunk_id]
+        elem_id = (block_idx % self._n_blocks) * self._block_size
+        offset = np.dtype(self._dtype).itemsize * elem_id
+        arr = np.frombuffer(
+            buffer, dtype=self._dtype, count=self._block_size, offset=offset
+        )
+        self._curr_idx += 1
+        return arr
+
+
