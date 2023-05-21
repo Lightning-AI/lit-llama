2772c08d1bec1456adf81b9f9e1b4d77a685710b
Carlos Mocholí <carlossmocholi@gmail.com>
2023-05-21 19:12:16 +0200

    Do not use symlinks when downloading for cloud support

--- scripts/download.py
+++ scripts/download.py
@@ -25,7 +25,7 @@ def download_from_hub(repo_id: Optional[str] = None, local_dir: str = "checkpoin
 
     from huggingface_hub import snapshot_download
 
-    snapshot_download(repo_id, local_dir=local_dir)
+    snapshot_download(repo_id, local_dir=local_dir, local_dir_use_symlinks=False)
 
 
 if __name__ == "__main__":
