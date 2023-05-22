8b753a55a7060958a03b6f23660757f3ee39f267
Sebastian Raschka <mail@sebastianraschka.com>
2023-05-22 08:22:59 -0400

    undo unnecessary code change

--- evaluate/adapter_v2.py
+++ evaluate/adapter_v2.py
@@ -92,12 +92,12 @@ def main(
             model = LLaMA.from_name(name)
             add_adapter_v2_parameters_to_linear_layers(model)
 
-            # 1. Load the pretrained weights
-            model.load_state_dict(pretrained_checkpoint, strict=False)
-            # 2. Load the fine-tuned adapter weights
-            model.load_state_dict(adapter_checkpoint, strict=False)
+        # 1. Load the pretrained weights
+        model.load_state_dict(pretrained_checkpoint, strict=False)
+        # 2. Load the fine-tuned adapter weights
+        model.load_state_dict(adapter_checkpoint, strict=False)
 
-            print(f"Time to load model: {time.time() - t0:.02f} seconds.", file=sys.stderr)
+    print(f"Time to load model: {time.time() - t0:.02f} seconds.", file=sys.stderr)
 
     model.eval()
 
