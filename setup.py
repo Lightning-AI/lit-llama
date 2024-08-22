2b47e104e006e898605984a0de0e1e2b7e61d72a
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2024-08-22 11:06:40 -0400

    fix requirements + aggregate setup & req. & config in pyproject (#453)

--- setup.py
+++ setup.py
@@ -1,28 +1,5 @@
 # Copyright Lightning AI. Licensed under the Apache License 2.0, see LICENSE file.
 
-import os
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-
-_PATH_ROOT = os.path.dirname(__file__)
-
-with open(os.path.join(_PATH_ROOT, "README.md"), encoding="utf-8") as fo:
-    readme = fo.read()
-
-setup(
-    name='lit-llama',
-    version='0.1.0',
-    description='Implementation of the LLaMA language model',
-    author='Lightning AI',
-    url='https://github.com/lightning-AI/lit-llama',
-    install_requires=[
-        "torch>=2.0.0",
-        "lightning @ git+https://github.com/Lightning-AI/lightning@master",
-        "sentencepiece",
-        "bitsandbytes",
-    ],
-    packages=find_packages(),
-    long_description=readme,
-    long_description_content_type="text/markdown",
-)
+setup()
